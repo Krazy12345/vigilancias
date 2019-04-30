71ecb313e9b6026951c0940da23f405f7e2bb0d6
Krazy12345 <17901andrecarvalho@aeplegua.pt>
2019-04-30 13:48:04 +0100

    update 1.0

--- estagio_2/estagio_2/Form5.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form5
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Me.Hide()
-        Form1.Show()
-
-    End Sub
-End Class
--- estagio_2/estagio_2/My Project/Application.Designer.vb
+++ estagio_2/estagio_2/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.estagio_2.Form1
+            Me.MainForm = Global.estagio_2.PaginaInicia
         End Sub
     End Class
 End Namespace
